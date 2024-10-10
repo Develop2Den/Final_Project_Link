03ee438a9c6939964b026edc8d870b84ab17edc7
AlexandrIsai <artat@ukr.net>
2024-10-10 23:06:50 +0200

    structure

---
+++ src/main/java/com/Final_Project/Linkedin/config/OpenAPIConfig.java
@@ -0,0 +1,27 @@
+package com.Final_Project.Linkedin.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Configuration class for setting up Swagger/OpenAPI documentation.
+ */
+@Configuration
+public class OpenAPIConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API Documentation")
+                        .version("1.0")
+                        .description("API documentation for the LinkedIn project")
+                        .contact(new Contact().name("Alexander Isai").email("alexanderisay@gmail.com")));
+    }
+}

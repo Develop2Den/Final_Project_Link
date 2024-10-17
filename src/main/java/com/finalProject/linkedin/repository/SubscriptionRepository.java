ebfcc150332fea708a968a50e875c3dc989dddda
Develop2Den <115218139+Develop2Den@users.noreply.github.com>
2024-10-17 10:42:40 +0300

    structure works

--- src/main/java/com/finalProject/linkedin/repository/SubscriptionRepository.java
+++ src/main/java/com/finalProject/linkedin/repository/SubscriptionRepository.java
@@ -1,9 +1,10 @@
 package com.finalProject.linkedin.repository;
 
+import com.finalProject.linkedin.entity.Subscription;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface SubscriptionRepository extends JpaRepository<SubscriptionRepository, Long> {
+public interface SubscriptionRepository extends JpaRepository<Subscription, Long> {
 
 }

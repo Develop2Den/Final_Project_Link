e6ec6c2e262a72e7dd5527f65fe105317cb2c376
egormyronchuk <egormyronchuk@gmail.com>
2024-10-19 21:28:16 +0300

    To push

--- src/main/java/com/finalProject/linkedin/controller/PostController.java
+++ src/main/java/com/finalProject/linkedin/controller/PostController.java
@@ -15,44 +15,50 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping(value = "/home")
+@RequestMapping(value = "/post")
 @Slf4j
 @Validated
 public class PostController {
     private final PostServiceImpl postServiceImp;
 
-    @PostMapping(value = "creatPost")
+    @PostMapping(value = "")
     @Operation(summary = "Create new Post", description = "Creates a new Post")
     @ApiResponse(responseCode = "201")
     public ResponseEntity<CreatePostResponse> createPost(CreatePostReq createPostReq) {
         return ResponseEntity.status(HttpStatus.CREATED).body(postServiceImp.creatPost(createPostReq));
     }
 
-    @DeleteMapping(value = "deletePost")
+    @DeleteMapping(value = "/{postId}")
     @Operation(summary = "Delete Post", description = "Mark profile as logically deleted by setting 'deletedAt'")
     @ApiResponse(responseCode = "200")
-    public ResponseEntity<Void> deletePost(Long postId) {
+    public ResponseEntity<Void> deletePost(@PathVariable Long postId) {
+        postServiceImp.deletePost(postId);
         return ResponseEntity.noContent().build();
     }
 
-    @GetMapping(value = "getPostById")
+    @GetMapping(value = "/{postId}")
     @Operation(summary = "Get Post", description = "Get posts by post id")
     @ApiResponse(responseCode = "200")
-    public ResponseEntity<CreatePostResponse> getPostByPostId(Long postId) {
+    public ResponseEntity<CreatePostResponse> getPostByPostId(@PathVariable Long postId) {
         return ResponseEntity.status(HttpStatus.CREATED).body(postServiceImp.getPostById(postId));
     }
 
-    @GetMapping(value = "getAllPostsForUser")
+    @GetMapping(value = "/{userId}")
     @Operation(summary = "Get post for User", description = "Get posts with pagination for User by his id")
     @ApiResponse(responseCode = "200")
-    public ResponseEntity<Page<CreatePostResponse>> getAllPostsForUser (Long useId ,int page, int size){
-        return ResponseEntity.ok(postServiceImp.getAllPostsForUser(useId,page,size));
+    public ResponseEntity<Page<CreatePostResponse>> getAllPostsForUser (
+            @PathVariable Long userId ,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size){
+        return ResponseEntity.ok(postServiceImp.getAllPostsForUser(userId,page,size));
     }
 
-    @GetMapping(value = "getAllPosts") // need to delete ? they need that ?
+    @GetMapping // need to delete ? they need that ?
     @Operation(summary = "Get post from base", description = "Get posts with pagination from base")
     @ApiResponse(responseCode = "204")
-    public ResponseEntity<Page<CreatePostResponse>> getAllPosts (int page, int size){
+    public ResponseEntity<Page<CreatePostResponse>> getAllPosts (
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size){
         return ResponseEntity.ok(postServiceImp.getAllPosts(page,size));
     }
 

84b5a30879b3aefe2a84e6bcf8861a3e17b0947b
Develop2Den <115218139+Develop2Den@users.noreply.github.com>
2024-10-19 17:06:25 +0300

    Feature favorite (#8)
    * added favorite
    
    * fixed files
    
    * fixed swagger config

--- src/main/java/com/finalProject/linkedin/config/SwaggerConfig.java
+++ src/main/java/com/finalProject/linkedin/config/SwaggerConfig.java
@@ -1,9 +1,6 @@
 package com.finalProject.linkedin.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
 import org.springframework.context.annotation.Configuration;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
@@ -15,12 +12,6 @@ import org.springframework.context.annotation.Bean;
 
 @Configuration
 @OpenAPIDefinition
-@SecurityScheme(
-        name = "cookieAuth",
-        type = SecuritySchemeType.APIKEY,
-        in = SecuritySchemeIn.COOKIE,
-        paramName = "JSESSIONID"
-)
 public class SwaggerConfig {
 
     @Bean

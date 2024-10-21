c3c7fa36a5a2b8c95d4bb425970ee1757df3a100
DolzhykovAlex <162730256+DolzhykovAlex@users.noreply.github.com>
2024-10-21 16:23:45 +0300

    Comment base logic. (#11)

--- src/main/java/com/finalProject/linkedin/controller/CommentController.java
+++ src/main/java/com/finalProject/linkedin/controller/CommentController.java
@@ -1,15 +1,47 @@
 package com.finalProject.linkedin.controller;
 
+import com.finalProject.linkedin.dto.request.comment.CreateCommentReq;
+import com.finalProject.linkedin.dto.responce.comment.CreateCommentRes;
+import com.finalProject.linkedin.service.serviceIR.CommentService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Slf4j
 @RestController
-@RequestMapping("/")
+@RequestMapping("/comments")
 @RequiredArgsConstructor
 @Validated
 public class CommentController {
+    private final CommentService commentService;
+
+    @GetMapping("/list")
+    public List<CreateCommentRes> getAllComments(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "5") int size
+    ) {
+        log.info("Pageable request chats: page={}, size={}", page, size);
+        Pageable pageable = PageRequest.of(page, size);
+        return commentService.findAll(pageable);
+    }
+
+
+    @PostMapping("/create")
+    public ResponseEntity<CreateCommentRes> createChat(@Valid @RequestBody CreateCommentReq createCommentReq) {
+        return ResponseEntity.ok(commentService.create(createCommentReq));
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Void> deleteChat(@PathVariable long id) {
+        if (commentService.deleteById(id)) return ResponseEntity.ok().build();
+        else return ResponseEntity.notFound().build();
+    }
+
 }

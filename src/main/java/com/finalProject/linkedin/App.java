19c96b6c682a9b59e4e978e5cdaa087d19deda9b
Develop2Den <115218139+Develop2Den@users.noreply.github.com>
2024-11-02 13:43:31 +0200

    Update App.java

--- src/main/java/com/finalProject/linkedin/App.java
+++ src/main/java/com/finalProject/linkedin/App.java
@@ -1,6 +1,6 @@
 package com.finalProject.linkedin;
 
-import io.github.cdimascio.dotenv.Dotenv;
+// import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;

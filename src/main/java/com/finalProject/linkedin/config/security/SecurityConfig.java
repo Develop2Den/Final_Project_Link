8244c18e5523361709cecaa52a3bf70191f87750
Develop2Den <115218139+Develop2Den@users.noreply.github.com>
2024-11-04 14:31:20 +0200

    Update SecurityConfig.java

--- src/main/java/com/finalProject/linkedin/config/security/SecurityConfig.java
+++ src/main/java/com/finalProject/linkedin/config/security/SecurityConfig.java
@@ -67,11 +67,11 @@ public class SecurityConfig {
                         .requestMatchers("/profiles/**").authenticated()
                         .anyRequest().authenticated()
                 )
-                .sessionManagement(session -> session
-                        .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
-                        .maximumSessions(1)
-                        .maxSessionsPreventsLogin(false)
-                )
+                // .sessionManagement(session -> session
+                //         .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
+                //         .maximumSessions(1)
+                //         .maxSessionsPreventsLogin(false)
+                // )
                 .oauth2Login(oauth2 -> oauth2
                         .loginPage("/login")
                         .successHandler(this::oauth2SuccessHandler)

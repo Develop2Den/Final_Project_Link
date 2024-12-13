4107b694db25d3f8344fe6d76109a393144db7a4
AlexandrIsai <artat@ukr.net>
2024-12-13 11:09:34 +0100

    update profile specification

--- src/main/java/com/finalProject/linkedin/service/specification/ProfileSpecification.java
+++ src/main/java/com/finalProject/linkedin/service/specification/ProfileSpecification.java
@@ -12,25 +12,30 @@ import org.springframework.data.jpa.domain.Specification;
  * @author Alexander Isai on 10.10.2024.
  */
 public class ProfileSpecification {
+
     public static Specification<Profile> hasEmail(String email) {
         return (Root<Profile> root, CriteriaQuery<?> query, CriteriaBuilder builder) ->
-                email == null ? null : builder.like(root.get("email"), "%" + email + "%");
+                (email == null || email.isBlank())
+                        ? builder.conjunction()
+                        : builder.like(root.get("email"), "%" + email + "%");
     }
 
     public static Specification<Profile> hasName(String name) {
         return (Root<Profile> root, CriteriaQuery<?> query, CriteriaBuilder builder) ->
-                name == null ? null : builder.like(root.get("name"), "%" + name + "%");
+                (name == null || name.isBlank())
+                        ? builder.conjunction()
+                        : builder.like(root.get("name"), "%" + name + "%");
     }
 
     public static Specification<Profile> hasSurname(String surname) {
         return (Root<Profile> root, CriteriaQuery<?> query, CriteriaBuilder builder) ->
-                surname == null ? null : builder.like(root.get("surname"), "%" + surname + "%");
+                (surname == null || surname.isBlank())
+                        ? builder.conjunction()
+                        : builder.like(root.get("surname"), "%" + surname + "%");
     }
 
     public static Specification<Profile> isNotDeleted() {
         return (Root<Profile> root, CriteriaQuery<?> query, CriteriaBuilder builder) ->
                 builder.isNull(root.get("deletedAt"));
     }
-
-
 }

981b59cb46e3bbad45bd41e813f48b7e53306871
AlexDDD <dolzhikov.alexander1@gmail.com>
2024-12-17 15:33:45 +0200

    Normalize notification controller, service.

--- src/main/java/com/finalProject/linkedin/controller/NotificationController.java
+++ src/main/java/com/finalProject/linkedin/controller/NotificationController.java
@@ -3,6 +3,7 @@ package com.finalProject.linkedin.controller;
 
 import com.finalProject.linkedin.dto.request.notification.NotificationReq;
 import com.finalProject.linkedin.dto.responce.notification.NotificationRes;
+import com.finalProject.linkedin.mapper.NotificationMapper;
 import com.finalProject.linkedin.service.serviceIR.NotificationService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -23,24 +24,24 @@ import java.util.List;
 
 public class NotificationController {
     private final NotificationService notificationService;
+    private final NotificationMapper notificationMapper;
 
-    @Operation(summary = "Get paginated notifications", description = "Get list of notifications with pagination")
+    @Operation(summary = "Show method not for production - Get paginated notifications", description = "Get list of notifications with pagination")
     @ApiResponse(responseCode = "200")
     @GetMapping("/list")
-    public List<NotificationRes> getAllNotification(
+    public ResponseEntity<List<NotificationRes>> getAllNotification(
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "5") int size
     ) {
-        log.info("Pageable request notifications: page={}, size={}", page, size);
         Pageable pageable = PageRequest.of(page, size);
-        return notificationService.findAll(pageable);
+        return ResponseEntity.ok(notificationService.findAll(pageable).map(notificationMapper::toNotificationRes).toList());
     }
 
     @Operation(summary = "Create new notification", description = "Creates a new notification")
     @ApiResponse(responseCode = "201")
     @PostMapping("/create")
     public ResponseEntity<NotificationRes> createNotification(@Valid @RequestBody NotificationReq notificationReq) {
-        return ResponseEntity.ok(notificationService.create(notificationReq));
+        return ResponseEntity.ok(notificationMapper.toNotificationRes(notificationService.create(notificationMapper.toNotification(notificationReq))));
     }
 
     @Operation(summary = "Mark notification as deleted", description = "Mark notification as logically deleted by setting 'deletedAt'")
@@ -54,47 +55,46 @@ public class NotificationController {
     @Operation(summary = "Get paginated notifications by user id", description = "Get list of notifications with pagination by user id")
     @ApiResponse(responseCode = "200")
     @GetMapping("/list/{id}")
-    public List<NotificationRes> getAllNotification(
+    public ResponseEntity<List<NotificationRes>> getAllNotification(
             @PathVariable long id,
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "5") int size
     ) {
-        log.info("Pageable request notifications by id: ID = {},  page={}, size={}", id, page, size);
         Pageable pageable = PageRequest.of(page, size);
-        return notificationService.findAll(pageable);
+        return ResponseEntity.ok(notificationService.findByIdAndDeleteFalse(id, pageable).map(notificationMapper::toNotificationRes).toList());
     }
 
     @Operation(summary = "Get number of unread notifications by user id", description = "Get number of unread notifications by user id sorted by created date")
     @ApiResponse(responseCode = "200")
     @GetMapping("/count/{id}")
-    public long getAllNotificationCount(
+    public ResponseEntity<Long> getAllNotificationCount(
             @PathVariable long id
     ) {
         log.info("Request count unread notifications by id: ID = {}", id);
-        return notificationService.countByRecipientIdReadFalse(id);
+        return ResponseEntity.ok(notificationService.countByRecipientIdReadFalse(id));
     }
 
     @Operation(summary = "Get paginated unread notifications by user id", description = "Get paginated unread notifications by user id sorted by created date")
     @ApiResponse(responseCode = "200")
     @GetMapping("/unread/{id}")
-    public List<NotificationRes> getAllUnreadNotification(
+    public ResponseEntity<List<NotificationRes>> getAllUnreadNotification(
             @PathVariable long id,
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "5") int size
     ) {
         log.info("Pageable request unread notification by id: ID = {},  page={}, size={}", id, page, size);
         Pageable pageable = PageRequest.of(page, size);
-        return notificationService.findByIdAndReadFalse(pageable, id);
+        return ResponseEntity.ok(notificationService.findByIdAndReadFalse(id, pageable).map(notificationMapper::toNotificationRes).toList());
     }
 
     @GetMapping("/{notificationId}")
     @Operation(summary = "Get notification by ID", description = "Get notification by its ID")
     @ApiResponse(responseCode = "200")
     public ResponseEntity<NotificationRes> getNotificationById(@PathVariable Long notificationId) {
-        return ResponseEntity.ok(notificationService.getNotificationById(notificationId));
+        return ResponseEntity.ok(notificationMapper.toNotificationRes(notificationService.getNotificationById(notificationId)));
     }
 
-    @PutMapping("/{notificationId}")
+    @PutMapping("/read/{notificationId}")
     @Operation(summary = "mark notification as read", description = "mark notification as read by its ID")
     @ApiResponse(responseCode = "200")
     public ResponseEntity<Void> setNotificationRead(@PathVariable Long notificationId) {

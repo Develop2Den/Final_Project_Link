ae37e4cc4042305d6afaab4c42e01d846cc00def
Develop2Den <115218139+Develop2Den@users.noreply.github.com>
2024-12-24 16:30:35 +0200

    Update SecurityConfig.java

--- src/main/java/com/finalProject/linkedin/config/security/SecurityConfig.java
+++ src/main/java/com/finalProject/linkedin/config/security/SecurityConfig.java
@@ -118,6 +118,18 @@ public class SecurityConfig {
     }
 
     @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+            CorsConfiguration configuration = new CorsConfiguration();
+            configuration.setAllowedOrigins(List.of(FRONT_URL));
+            configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+            configuration.setAllowedHeaders(List.of("*"));
+            configuration.setAllowCredentials(true);
+            UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+            source.registerCorsConfiguration("/**", configuration);
+        return source;
+}
+
+    @Bean
     public LogoutSuccessHandler customLogoutSuccessHandler() {
         return (HttpServletRequest req, HttpServletResponse res, Authentication authentication) -> {
             res.setStatus(OK.value());
